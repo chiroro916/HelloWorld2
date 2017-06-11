b5b828a05bc188bf49ca84a506f0c0eda15935fc
Chihiro <Chihiro@M.local>
2017-06-11 20:18:22 +0900

    Initial Commit

---
+++ HelloWoeld_2/ViewController.swift
@@ -0,0 +1,66 @@
+//
+//  ViewController.swift
+//  HelloWoeld_2
+//
+//  Created by Chihiro Murakami on 2017/03/26.
+//  Copyright © 2017年 Chihiro Murakami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func tapWhite(_ sender: Any) {
+    view.backgroundColor = UIColor.white        }
+    
+    @IBAction func tapGreen(_ sender: AnyObject) {
+        view.backgroundColor = UIColor.green    }
+    
+    @IBAction func tapRed(_ sender: Any) {
+        view.backgroundColor = UIColor.red      }
+    
+    @IBAction func tapBlue(_ sender: Any) {
+        view.backgroundColor = UIColor.blue     }
+
+    @IBAction func tapYellow(_ sender: Any) {
+        view.backgroundColor = UIColor.yellow    }
+    
+    @IBAction func tapBlack(_ sender: Any) {
+        view.backgroundColor = UIColor.black    }
+    
+    @IBAction func tapBrown(_ sender: Any) {
+        view.backgroundColor = UIColor.brown    }
+    
+    @IBAction func tapCyan(_ sender: Any) {
+        view.backgroundColor = UIColor.cyan    }
+    
+    @IBAction func tapMagenta(_ sender: Any) {
+        view.backgroundColor = UIColor.magenta    }
+    
+    @IBAction func tapOrange(_ sender: Any) {
+        view.backgroundColor = UIColor.orange    }
+
+    @IBAction func tapGrey(_ sender: Any) {
+        view.backgroundColor = UIColor.gray    }
+    
+    @IBAction func tapPurple(_ sender: Any) {
+        view.backgroundColor = UIColor.purple   }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
